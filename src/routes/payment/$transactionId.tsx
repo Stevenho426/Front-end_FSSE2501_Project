5626fd7a7b0fde2c1f364535b878ba19ec5248be
Stevenho426 <stevenho426@gmail.com>
2025-07-17 23:28:26 +0800

    2025/7/17 - final version_1

--- src/routes/payment/$transactionId.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import Payment from "../../ui/page/CheckoutPage/component/Payment.tsx";
-
-export const Route = createFileRoute('/payment/$transactionId')({
-  component: Payment,
-})
-
